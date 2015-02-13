23aad663adefee97c87b5b9ce06c8fc8259850db
Lars Kellogg-Stedman <lars@redhat.com>
2015-02-12 21:35:01 -0500

    fixed error in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as fd:
     setup(name='undocker',
           version='1',
-          py_modules=['undocker.py'],
+          py_modules=['undocker'],
           entry_points={
               'console_scripts': [
                   'undocker = undocker:main',
