f1d44c64ff9aa044d6ca43cee4993ffb6aefe7f2
Lars Kellogg-Stedman <lars@redhat.com>
2015-02-13 09:15:10 -0500

    correctly handle image archives
    a docker "image" produced by "docker save" can actually contain multiple
    images.  We now handle that correctly, and also have facilities for
    listing image layers and extracting specific layers rather than an
    entire image.

--- undocker.py
+++ undocker.py
@@ -16,12 +16,13 @@ LOG = logging.getLogger(__name__)
 
 def parse_args():
     p = argparse.ArgumentParser()
-    p.add_argument('--tag', '-t',
-                   default='latest')
+
     p.add_argument('--ignore-errors', '-i',
-                   action='store_true')
+                   action='store_true',
+                   help='Ignore OS errors when extracting files')
     p.add_argument('--output', '-o',
-                   default='.')
+                   default='.',
+                   help='Output directory (defaults to ".")')
     p.add_argument('--verbose', '-v',
                    action='store_const',
                    const=logging.INFO,
@@ -30,6 +31,14 @@ def parse_args():
                    action='store_const',
                    const=logging.DEBUG,
                    dest='loglevel')
+    p.add_argument('--list', '--ls',
+                   action='store_true',
+                   help='List layers in an image')
+    p.add_argument('--layer', '-l',
+                   action='append',
+                   help='Extract only the specified layer')
+    p.add_argument('image')
+
     p.set_defaults(level=logging.WARN)
     return p.parse_args()
 
@@ -49,9 +58,15 @@ def find_layers(img, id):
         for layer in find_layers(img, pid):
             yield layer
 
+
 def main():
     args = parse_args()
     logging.basicConfig(level=args.loglevel)
+
+    try:
+        name, tag = args.image.split(':', 1)
+    except ValueError:
+        name, tag = args.image, 'latest'
 
     with tempfile.NamedTemporaryFile() as fd:
         fd.write(sys.stdin.read())
@@ -60,22 +75,40 @@ def main():
             repos = img.extractfile('repositories')
             repos = json.load(repos)
 
-            top = repos[repos.keys()[0]][args.tag]
-            LOG.info('extracting image %s', top)
+            try:
+                top = repos[name][tag]
+            except KeyError:
+                LOG.error('failed to find image %s with tag %s',
+                          name,
+                          tag)
+                sys.exit(1)
+
+            LOG.info('extracting image %s (%s)', name, top)
             layers = list(find_layers(img, top))
+
+            if args.list:
+                print '\n'.join(reversed(layers))
+                sys.exit(0)
 
             if not os.path.isdir(args.output):
                 os.mkdir(args.output)
 
             for id in reversed(layers):
+                if args.layer and not id in args.layer:
+                    continue
+
                 LOG.info('extracting layer %s', id)
                 with tarfile.TarFile(
                         fileobj=img.extractfile('%s/layer.tar' % id)) as layer:
                     try:
                         layer.extractall(path=args.output)
-                    except OSError:
-                        if not args.ignore_errors:
-                            raise
+                    except OSError as exc:
+                        if args.ignore_errors:
+                            LOG.info('ignoring error: %s',
+                                     exc)
+                            continue
+
+                        raise
 
 
 if __name__ == '__main__':
