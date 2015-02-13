7de4eb26c2b3717bc663c99311bbd19fc0164284
Lars Kellogg-Stedman <lars@redhat.com>
2015-02-13 09:20:03 -0500

    inc version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 with open('requirements.txt') as fd:
     setup(name='undocker',
-          version='1',
+          version='2',
           py_modules=['undocker'],
           entry_points={
               'console_scripts': [
