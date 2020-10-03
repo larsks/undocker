649f3fdeb0a9cf8aa794d90d6cc6a7c7698a25e6
Lars Kellogg-Stedman <lars@oddbit.com>
2020-10-03 17:13:20 -0400

    fix missing description on pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
 
+# from https://packaging.python.org/guides/making-a-pypi-friendly-readme/
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='undocker',
         author = 'Lars Kellogg-Stedman',
         author_email = 'lars@oddbit.com',
-        version='6',
+        version='7',
         description='Unpack docker images',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
         url='http://github.com/larsks/undocker',
         py_modules=['undocker'],
         entry_points={
